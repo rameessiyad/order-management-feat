0358ce09991fab2281d37eccf8f2cb99b6cbd98a
rameessiyad <rameesmsiyadofficial786@gmail.com>
2026-08-06 07:48:16 +0530

    fix: dist file

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,12 +1,13 @@
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from 'generated/prisma/client';
+import 'dotenv/config';
 
 const adapter = new PrismaMariaDb({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'order_management',
+  host: process.env.DB_HOST!,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB_NAME!,
   connectionLimit: 5,
 });
 
